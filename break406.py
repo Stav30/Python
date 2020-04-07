66b267e4d8d309cb41e65af690d3d6760fdc320f
Stav <srammos1@gmail.com>
2020-04-07 13:36:58 -0400

    type: style
    more detailed instructions provided

--- break406.py
+++ break406.py
@@ -1,11 +1,12 @@
 """
 break statement causes immediate exit from loop
-promput user to 'Enter name: '
-when user enters stop for name --> exit the loop using break stmt
+1- prompt user to 'Enter name: ' and assign this value to variable name.
+2- if user enters stop for name --> exit the loop using break stmt
+3- prompt user to 'Enter age: ' and store value in age
+4- use print function with 'Hello', name, '=>', int(age)**2
 """
 while True:
     name = input('Enter name: ')
     if name == 'stop': break
     age = input('Enter age: ')
     print('Hello', name, '=>', int(age)**2)
-    
