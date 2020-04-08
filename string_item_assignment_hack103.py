6591b03c38d1164337e3824625bae5d01d9e7a26
Stav <srammos1@gmail.com>
2020-04-07 21:58:49 -0400

    Initial commit

---
+++ string_item_assignment_hack103.py
@@ -0,0 +1,14 @@
+"""
+103
+Convert a string to a lists
+expand the list
+change the list in place
+join the new char to the list w/empty delimiter
+"""
+S = 'shrubbery'
+print(S)
+L = list(S)
+print(L)
+L[1]='c'
+''.join(L)
+print(L)
